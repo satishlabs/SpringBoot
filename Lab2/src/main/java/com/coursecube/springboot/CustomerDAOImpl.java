3e47a653c361342c065ce27821f348527a1eb6aa
Admin <spd61185@gmail.com>
2020-03-28 22:11:51 +0530

    Lab2 : Spring Boot – JDBC ( Your First Boot App) Working Steps 1) Copy Lab1 as Lab2 2) Update pom.xml with Spring Boot starters. 3) Select Project => maven => Update Project. 4) Remove JLCConfig.java ( Bean Configuration class) 5) Create the Source folder called src/main/resoures 6) Place application.properties in src/main/resoures 7) Update application.properties with required Properties. 8) Update MyBootApplication.java (Boot Application Main Class) 9) Following Files are as Lab1. Customer.java CustomerDAO.java JdbcCustomerDAO.java CustomerRowMapper.java

---
+++ Lab2/src/main/java/com/coursecube/springboot/CustomerDAOImpl.java
@@ -0,0 +1,25 @@
+package com.coursecube.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("custDAO")
+public class CustomerDAOImpl implements CustomerDAO{
+	@Autowired
+	JdbcTemplate jdbcTemp;
+	
+	public void addCustomer(Customer cust) {
+		String sql = "insert into customers values(?,?,?,?,?)"; 
+		jdbcTemp.update(sql, cust.getCid(), cust.getCname(), cust.getEmail(), cust.getPhone(), cust.getCity()); 
+	}
+
+	public List<Customer> getAllCustomer() {
+		String sql = "select * from customers"; 
+		List<Customer> list = jdbcTemp.query(sql, new CustomerRowMapper()); 
+		return list; 
+	}
+
+}
---
+++ Lab2/src/main/java/com/coursecube/springboot/MyBootApplication.java
@@ -0,0 +1,45 @@
+package com.coursecube.springboot;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(scanBasePackages="com.coursecube.springboot")
+public class MyBootApplication implements CommandLineRunner 
+{
+	private static final Logger log = LoggerFactory.getLogger(MyBootApplication.class); 
+	@Autowired
+	CustomerDAO cdao;
+    public static void main( String[] args )
+    {
+       SpringApplication.run(MyBootApplication.class, args);
+    }
+
+	public void run(String... args) throws Exception {
+		log.info("My Boot Application...Starts");
+		log.debug("My Boot Application...Starts");
+		log.info("--------------------------------");
+		
+		//1 Add Customer
+		Customer cust1 = new Customer(919, "Satish", "sati@sap.com", 89765, "Ranchi");
+		cdao.addCustomer(cust1);
+		
+		//2. get All Customers
+		//System.out.println("getAllCustomers");
+		List<Customer> cList = cdao.getAllCustomer();
+		cList.forEach(cust->System.out.println(cust));
+		
+		log.info("---------------------------------------");
+		log.info("My Boot Application...Ends");
+		log.debug("My Boot Application...Ends");
+	}
+}
